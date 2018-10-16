e20f19ec36ca311d6856796b68ea892e5ecc384c
Aasish Adhikari <gemini.aasis@gmail.com>
2018-10-16 21:22:29 +0930

    rebased project to previous commit

---
+++ ETennis2/Data/Migrations/20181016110840_identityError.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ETennis2.Data.Migrations
+{
+    public partial class identityError : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
