836427b781734fecb497b8b4bac53e1f4d5990d1
Alfonse12 <mudosamugisho@gmail.com>
2018-10-18 10:28:31 +0930

    images change
    work on UI

---
+++ ETennis2/Data/Migrations/20181017015848_ETennis2.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ETennis2.Data.Migrations
+{
+    public partial class ETennis2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedule_EventId",
+                table: "Schedule",
+                column: "EventId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedule_MemberId",
+                table: "Schedule",
+                column: "MemberId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedule_Event_EventId",
+                table: "Schedule",
+                column: "EventId",
+                principalTable: "Event",
+                principalColumn: "EventId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Schedule_Member_MemberId",
+                table: "Schedule",
+                column: "MemberId",
+                principalTable: "Member",
+                principalColumn: "MemberId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedule_Event_EventId",
+                table: "Schedule");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Schedule_Member_MemberId",
+                table: "Schedule");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Schedule_EventId",
+                table: "Schedule");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Schedule_MemberId",
+                table: "Schedule");
+        }
+    }
+}
